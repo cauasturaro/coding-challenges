d5b50351f12fe4529f2c8e5c66025f4007737db5
cauasturaro <caua.sturaro111@gmail.com>
2025-10-20 02:05:20 -0300

    hackerrank: easy - dynamic-array

---
+++ HackerRank/Data Structures/Easy/dynamic-array.py
@@ -0,0 +1,26 @@
+# Problem: Dynamic Array using queries
+# Platform: HackerRank
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/dynamic-array/problem
+# -----------------------------------------------------
+# Time Complexity: O(n + q) q: query ammount
+# Space Complexity: O(n + q)
+# -----------------------------------------------------
+
+def dynamicArray(n, queries):
+    lastAnswer = 0
+    arr = [[] for _ in range(n)]
+    answers = []
+    
+    for q in queries:
+        idx = (q[1] ^ lastAnswer) % n
+        if q[0] == 1:
+            arr[idx].append(q[2])
+        if q[0] == 2:
+            arr_size = len(arr[idx])
+            eidx = q[2] % arr_size
+            
+            lastAnswer = arr[idx][eidx]            
+            answers.append(lastAnswer)
+            
+    return answers
