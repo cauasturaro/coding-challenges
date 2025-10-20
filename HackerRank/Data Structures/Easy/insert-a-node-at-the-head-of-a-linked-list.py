157e135eb75322ba13e41ebd24fd6792255c18fd
cauasturaro <caua.sturaro111@gmail.com>
2025-10-20 02:59:21 -0300

    hackerrank: easy - insert-a-node-at-the-head-of-a-linked-list

--- HackerRank/Data Structures/Easy/insert-a-node-at-the-head-of-a-linked-list.py
+++ HackerRank/Data Structures/Easy/insert-a-node-at-the-head-of-a-linked-list.py
@@ -3,7 +3,7 @@
 # Difficulty: Easy
 # Link: https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem
 # -----------------------------------------------------
-# Time Complexity: O(n)
+# Time Complexity: O(1)
 # Space Complexity: O(1)
 # -----------------------------------------------------
 
@@ -13,18 +13,11 @@
 #     int data
 #     SinglyLinkedListNode next
 #
+#
+def insertNodeAtHead(llist, data):
+    node = llist
+    llist = SinglyLinkedListNode(data) 
+    llist.next = node
+    return llist
 
-def insertNodeAtTail(head, data):
-    node = head
-    if not node:
-        node = SinglyLinkedListNode(data)
-        head = node
-        return head
-    
-    while node.next:
-        node = node.next
-    newNode = SinglyLinkedListNode(data)
-    node.next = newNode
-    
-    return head
      
