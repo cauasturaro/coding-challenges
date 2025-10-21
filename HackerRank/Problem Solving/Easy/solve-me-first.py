50666391011a66378911a2a79f1f8fb1fc2b9bf2
cauasturaro <caua.sturaro111@gmail.com>
2025-10-21 04:56:58 -0300

    hackerrank easy - problem solving - solve-me-first

---
+++ HackerRank/Problem Solving/Easy/solve-me-first.py
@@ -0,0 +1,17 @@
+# Problem: Solve me First (a sum function)
+# Platform: HackerRank
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+# -----------------------------------------------------
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+# -----------------------------------------------------
+
+def solveMeFirst(a,b):
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
